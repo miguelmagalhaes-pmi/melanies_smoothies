856c1d8908f62a0710f3cce031a3663ed22651f2
miguelmagalhaes-pmi <miguel.magalhaes@neurogum.com>
2026-07-07 15:37:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,8 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit Order')
     st.write(my_insert_stmt)
+    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+    st.text(smoothiefroot_response)
     
     if time_to_insert:
     
